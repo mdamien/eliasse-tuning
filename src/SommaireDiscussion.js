39e0a7f9cf0fc0504e6fdb3436659545175ea7e2
Damien <damien@dam.io>
2020-11-12 17:13:35 +0100

    simplify derouleur

--- src/SommaireDiscussion.js
+++ src/SommaireDiscussion.js
@@ -36,35 +36,23 @@ function SommaireDiscussion() {
 
   return (
    <div style={{padding:10}}>
-    <ul>
-      {DATA.discussion ? DATA.discussion.divisions.map((div, i) => <div key={div.position}>
-        <li>
-           <strong>- {div.place}</strong>
-           <ul>
-             {DATA.amdts_derouleur ? DATA.amdts_derouleur.map(amdt => {
-               var amdt_span = <span>
-                  Amdt n°{amdt.numero} de {amdt.auteurLabel} 
-                  {amdt.auteurGroupe ? <span> ({amdt.auteurGroupe})</span> : null}
-               </span>
-               if (DATA.prochainADiscuter.numAmdt === amdt.numero) {
-                amdt_span = <strong>{amdt_span} (en discussion)</strong>
-               }
-               return (DATA.discussion.divisions[i+1] 
-                     && comparePositions(amdt.position, div.position))
-                     && comparePositions(DATA.discussion.divisions[i+1].position, amdt.position) ? 
-                  <li onClick={selectAmdt.bind(null, amdt.numero)} key={amdt.numero}>
-                    {DATA.amendements[currAmdtIndex()].numero === amdt.numero ?
-                      <u ref={selectedAmdtRef}>{amdt_span}</u>
-                    : amdt_span
-                    }
-                  </li>
-               : null
-             }) : null}
-           </ul>
-         </li>
-         <br/>
-         </div>) : null}
-    </ul>
+      <ul>
+       {DATA.amdts_derouleur ? DATA.amdts_derouleur.map(amdt => {
+         var amdt_span = <span>
+            Amdt n°{amdt.numero} de {amdt.auteurLabel} 
+            {amdt.auteurGroupe ? <span> ({amdt.auteurGroupe})</span> : null}
+         </span>
+         if (DATA.prochainADiscuter.numAmdt === amdt.numero) {
+          amdt_span = <strong>{amdt_span} - en discussion</strong>
+         }
+         return <li onClick={selectAmdt.bind(null, amdt.numero)} key={amdt.numero}>
+              {DATA.amendements[currAmdtIndex()].numero === amdt.numero ?
+                <u ref={selectedAmdtRef}>{amdt_span}</u>
+              : amdt_span
+              }
+            </li>
+       }) : null}
+   </ul>
    </div>
   );
 }
