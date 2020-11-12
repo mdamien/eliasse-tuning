57d736f5a0430fb39377f4cae0940647ca563404
Damien <damien@dam.io>
2020-11-12 16:16:53 +0100

    fine tuning

--- src/Amendement.js
+++ src/Amendement.js
@@ -4,8 +4,8 @@ function Amendement(props) {
   var data = props.data;
   return (
     <div className="amendement">
-      <a href={'http://www.assemblee-nationale.fr' + data.urlPDF}>PDF</a>
-      {(data.numero === DATA.prochainADiscuter.numAmdt ? <center><strong><u>Amendement en discussion</u></strong></center> : '')}
+      <a className="lien-pdf" href={'http://www.assemblee-nationale.fr' + data.urlPDF}>PDF</a>
+      {(data.numero === DATA.prochainADiscuter.numAmdt ? <div className="en-discussion"><strong>En discussion</strong></div> : '')}
       <center><h1>Amendement n°{data.numero}</h1></center>
       <center><p>de <span dangerouslySetInnerHTML={{__html: data.listeDesSignataires}}></span></p></center>
       {data.sortEnSeance ? <center><p><strong>Sort: {data.sortEnSeance}</strong></p></center> : null}
